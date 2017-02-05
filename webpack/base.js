9c159253fa4c4664b563795d31f74ee5e7fa845a
Dimitar Kolev <dick@ingbg.com>
2017-02-06 00:24:49 +0200

    Refactor

---
+++ webpack/base.js
@@ -0,0 +1,40 @@
+module.exports = function (mikser) {
+	var config = {
+		devtool: '#source-map',
+		target: 'node',
+		resolve: {
+			modules: [mikser.options.workingFolder, 'node_modules'],
+			extensions: ['.js', '.vue'],
+			alias: {
+				'layouts': mikser.config.layoutsFolder,
+				'files': mikser.config.filesFolder,
+				'shared': mikser.config.sharedFolder,
+				'runtime': mikser.config.runtimeFilesFolder,
+				'plugins': mikser.config.pluginsFolder,
+			}
+		},
+		module: {
+			rules: [{
+				test: mikser.config.layoutsFolder,
+				loader: 'front-matter-loader?onlyBody'
+			},{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				options: {
+					loaders: {
+						js: 'babel-loader',
+						scss: 'css-loader'
+					}
+				}
+			},{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				query: {
+					presets: ['es2015', 'stage-2']
+				},
+			}]
+		},
+	}
+	return config
+}
