0f337dd486bc8ab35d3f8d1f64bc1c416f71cf58
Dimitar Kolev-Dick <dick@ingbg.com>
2019-06-28 15:17:28 +0300

    Rollback

--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const base = require('./webpack.base.config')
-const nodeExternals = require('webpack-node-externals')
 const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
 const path = require('path');
 
@@ -24,7 +23,6 @@ module.exports = function (mikser, layout) {
 			maxAssetSize: Infinity
 		},
 		externals: {
-			canvas: "commonjs canvas"
 		},
 		plugins: [
 			new webpack.DefinePlugin({
